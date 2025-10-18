a2bc05d30b1ee0210289e08957685e86ba57b0f2
YOUSIF10-sf <easyvaletjo@gmail.com>
2025-10-18 10:48:47 +0300

    Update AppBody.tsx

--- src/app/components/AppBody.tsx
+++ src/app/components/AppBody.tsx
@@ -4,12 +4,13 @@ import { useTranslation } from '../context/TranslationContext';
 import Header from './Header';
 
 export default function AppBody({ children }) {
+  // @ts-ignore
   const { language } = useTranslation();
 
   return (
     <body className={language === 'ar' ? 'rtl' : 'ltr'}>
-        <Header />
-        {children}
+      <Header />
+      {children}
     </body>
   );
 }
